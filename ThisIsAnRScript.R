cb2f705b76f0287bfe44a9916bed5418c331f84a
Adam Guenoun <adguenoun@gmail.com>
2025-01-16 12:42:46 -0600

    TexasGoBrrr Changes Deleted gayer sex
    ThisIsAnRScript Changes
    switched from squared to 101st
    switched exponent to 101

--- ThisIsAnRScript.R
+++ ThisIsAnRScript.R
@@ -1,8 +1,10 @@
 ## This is a R script 
-## Run this code to make a plot of x and x^2 for the integers from 1 to 10
+## Run this code to make a plot of x and x^101 for the integers from 1 to 10
 
 x <- 1:20
-y <- x^2
+y <- x^101
 
 
-plot(x,y, type = "b", main = "Squared integers")
+plot(x,y, type = "b", main = "101st integers")
+
+
